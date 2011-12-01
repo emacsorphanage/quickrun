8eaa692a8f0e6c63b44c7f938040ba237cc95184
Syohei YOSHIDA <syohex@gmail.com>
2011-12-01 23:38:37 +0900

    Add user definition parameter samples

---
+++ sample/sample_c11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+(quickrun-add-parameter "c++/c11"
+                        '((:command . "g++")
+                          (:compile . "%c -std=c++0x %o -o %n %s")
+                          (:exec    . "%n %a")
+                          (:remove  . ("%n")))
+                        :default "c++")
+*/
+
+int main ()
+{
+    std::vector <std::string> lst = { "a", "b", "c", "d" };
+
+    for (auto x : lst){
+        std::cout << "[" << x << "]" << std::endl;
+    }
+
+    return 0;
+}
