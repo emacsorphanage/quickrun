bb7cc933c9a17e99071b0014b8d2458644b3857c
Syohei YOSHIDA <syohex@gmail.com>
2012-06-13 23:55:36 +0900

    Add fortran and jsx sample

---
+++ sample/sample.jsx
@@ -0,0 +1,23 @@
+class HelloEditor {
+    var _editors : string[];
+
+    function constructor () {
+    }
+
+    function constructor (editors : string[]) {
+        this._editors = editors;
+    }
+
+    function hello () : void {
+        this._editors.forEach(function(e) {
+            log e;
+        });
+    }
+}
+
+class _Main {
+    static function main(args :string[]) : void {
+        var he = new HelloEditor(['Emacs', 'Vim', 'TextMate']);
+        he.hello();
+    }
+}
