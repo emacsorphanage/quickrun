00a29c46152e746850ffed7c455f2f0923f8ad13
Syohei YOSHIDA <syohex@gmail.com>
2013-11-04 11:16:28 +0900

    Bump up version and update Changes

--- quickrun.el
+++ quickrun.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-quickrun
-;; Version: 1.9.5
+;; Version: 1.9.6
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
