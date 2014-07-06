5a0f1a1dfb155d698659375547185bee37c21d02
Syohei YOSHIDA <syohex@gmail.com>
2014-07-06 11:41:41 +0900

    Enable compilation-mode when errors occured(#25)

--- quickrun.el
+++ quickrun.el
@@ -554,7 +554,7 @@ if you set your own language configuration.
         (process-send-region process (point-min) (point-max))
         (process-send-eof process)))))
 
-(defun quickrun/exec (cmd-lst)
+(defun quickrun/exec (cmd-lst src mode)
   (if quickrun/run-in-shell
       (quickrun/send-to-shell cmd-lst)
     (ignore-errors
@@ -567,7 +567,7 @@ if you set your own language configuration.
           (let ((file (quickrun/stdin-file-name)))
             (quickrun/send-file-as-stdin process file)))
         (set-process-sentinel process
-                              (quickrun/make-sentinel rest-cmds outputter))))))
+                              (quickrun/make-sentinel rest-cmds outputter src mode))))))
 
 (defvar quickrun/eshell-buffer-name "*eshell-quickrun*")
 (defvar quickrun/shell-last-command)
@@ -777,15 +777,29 @@ if you set your own language configuration.
           (funcall outputter-func)
           (setq buffer-read-only t))))))
 
-(defun quickrun/apply-colorizing ()
+(defun quickrun/apply-compilation-mode (input-file mode)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((case-fold-search nil))
+      (while (search-forward input-file nil t)
+        (replace-match quickrun/executed-file)))
+    (compilation-mode mode)))
+
+(defun quickrun/apply-colorizing (input-file mode)
   (setq buffer-read-only nil)
+  (when (and quickrun/executed-file input-file
+             (not (string= input-file quickrun/executed-file)))
+    (quickrun/apply-compilation-mode input-file mode))
   (quickrun/default-outputter)
+  (goto-char (point-min))
   (setq buffer-read-only t))
 
-(defun quickrun/make-sentinel (cmds outputter)
+(defun quickrun/make-sentinel (cmds outputter src mode)
   (lexical-let ((rest-commands cmds)
-                (outputter-func outputter))
-    (lambda (process state)
+                (outputter-func outputter)
+                (input src)
+                (orig-mode mode))
+    (lambda (process _event)
       ;; XXX Why reset `quickrun-option-outputter' ??
       (setq quickrun-option-outputter outputter-func)
       (when (memq (process-status process) '(exit signal))
@@ -793,10 +807,10 @@ if you set your own language configuration.
         (delete-process process)
         (let ((is-success (zerop (process-exit-status process))))
           (cond ((and is-success rest-commands)
-                 (quickrun/exec rest-commands))
+                 (quickrun/exec rest-commands input orig-mode))
                 (t
                  (if (not is-success)
-                     (quickrun/apply-colorizing)
+                     (quickrun/apply-colorizing input orig-mode)
                    (quickrun/apply-outputter outputter-func)
                    (run-hooks 'quickrun-after-run-hook))
                  (when (> scroll-conservatively 0)
@@ -1178,7 +1192,8 @@ by quickrun.el. But you can register your own command for some languages")
                  (quickrun/compilation-start cmd compile-conf)))
               (t
                (quickrun/setup-exec-buffer)
-               (quickrun/exec (gethash :exec cmd-info-hash))
+               (quickrun/exec (gethash :exec cmd-info-hash)
+                              (file-name-nondirectory src) major-mode)
                (unless (quickrun/defined-outputter-p quickrun-option-outputter)
                  (quickrun/popup-output-buffer))))))))
 
